2fff0fda1bfc46c83e0996e591aaba5c1cf9d0be
dongwoo2 <ieiedong@naver.com>
2024-02-14 22:34:02 +0900

    1차 게시판 CRUD 완료

--- board/models.py
+++ board/models.py
@@ -8,7 +8,7 @@ class Board(models.Model):
     writer = models.CharField(max_length = 30)
     content = models.TextField()
     regdate = models.DateTimeField(auto_now=timezone.now)
-    readcount = models.IntegerField(defalut=0)
+    readcount = models.IntegerField(default=0)
     
     def __str__(self):
         return '%s. %s(%d)' % (self.title, self.writer, self.readcount)
--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 def index(request):
@@ -7,7 +7,7 @@ def index(request):
 from.models import Board
 
 def list(request): #list 라는 뷰함수를 만들어서
-    board_list = Board.objects.all()  # 보드모델로부터 전체목록을 가져와서
+    board_list = Board.objects.all().order_by('-id')  # 보드모델로부터 전체목록을 가져와서
     context = { #그걸 응답에 사용할 수 있게 딕셔너리를 만들고
         'board_list':board_list,
     }
@@ -17,3 +17,37 @@ def list(request): #list 라는 뷰함수를 만들어서
         'board/list.html',
         context
     )
+    
+def read(request, id): # 경로변수로 id를 하나 가져옴
+    board = Board.objects.get(pk=id) # board 모델에서 get으로 검색
+    board.incrementReadCount()
+    return render(request, 'board/read.html', {'board':board}) # 검색해서 나온 결과물로 html에 전달
+
+def regist(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        writer = request.POST.get('writer')
+        content = request.POST['content']
+        Board(title=title, writer=writer, content=content).save()
+        return redirect(reverse('board:list'))
+    else:
+        return render(request, 'board/regist.html')
+        
+def edit(request, id):
+    board = Board.objects.get(pk=id) # 검색된 보드를 수정해줌
+    if request.method == 'POST':
+        board.title = request.POST['title'] # 값이 없으면 에러가 나고
+        board.writer = request.POST.get('writer', 'default') # 값이 없으면 default를 쓰겠다
+        board.content = request.POST['content']
+        board.save() #수정하고 나서 수정된 정보를 save해야 해당 id와 동일한정보가 database에서 수정됨
+        return redirect(reverse('board:read', args=(id,)))
+    else:
+        return render(request, 'board/edit.html', {'board:board'})
+    
+def remove(request, id):
+    board = Board.objects.get(pk=id)
+    if request.method == 'POST':
+        board.delete()
+        return redirect(reverse('board:list'))
+    else:
+        return render(request, 'board/remove.html', {'board:board'})
